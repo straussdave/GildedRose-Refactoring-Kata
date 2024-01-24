8e9bf032ffe589af787376e29cff95d733ba9bee
straussdave <davidstrauss002@gmai.com>
2024-01-24 11:14:08 +0100

    Created Unit Test to check base value Setter and Getter functionality

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Net.NetworkInformation;
 using GildedRoseKata;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 
 namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
+    //All `items` have a `SellIn` value which denotes the number of days we have to sell the `items`
+    //All `items` have a `Quality` value which denotes how valuable the item is
     [Test]
-    public void Foo()
+    public void GetSetBaseValues()
     {
         var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-        var app = new GildedRose(items);
-        app.UpdateQuality();
         Assert.AreEqual("foo", items[0].Name);
+        Assert.AreEqual(0, items[0].SellIn);
+        Assert.AreEqual(0, items[0].SellIn);
     }
 }
