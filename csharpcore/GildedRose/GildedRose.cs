c3746cc8fd5793ca9251a892b1d05e0488baf0c7
straussdave <davidstrauss002@gmai.com>
2024-10-13 15:34:17 +0200

    microsoft/Phi-3-mini-4k-instruct​

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -1,83 +1,120 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GildedRoseKata
 {
     public class GildedRose
     {
-        internal IList<Item> Items;
+        IList<Item> Items;
 
         public GildedRose(IList<Item> items)
         {
-            this.Items = items;
+            Items = items ?? throw new ArgumentNullException(nameof(items));
         }
 
         public void UpdateQuality()
         {
+            // Update non-unique rarity items
+            UpdateNonUniqueItemsQuality();
+
+            // Update unique non-rare items
+            UpdateNonUniqueNonRareItemsQuality();
+
+            // Update rare items based on their sell-in timer
+            UpdateRareItemsQuality();
+
+            // Decrease sell-in for all items
+            Items.ForEach(item =>
+            {
+                if (item.IsNonRare)
+                {
+                    item.SellIn = Math.Max(item.SellIn - 1, 0);
+                }
+            });
+        }
+
+        private void UpdateNonUniqueItemsQuality()
+        {
+            var uniqueRarities = new HashSet<string> { "Aged Brie", "Backstage passes to a TAFKAL80ETC concert" };
             foreach (var item in Items)
             {
-                UpdateSellIn(item);
-                UpdateQuality(item);
-            }
-        }
-
-        private void UpdateSellIn(Item item)
-        {
-            if (item.Name != "Sulfuras, Hand of Ragnaros")
-            {
-                item.SellIn--;
-            }
-        }
-
-        private void UpdateQuality(Item item)
-        {
-            if (item.Quality == 0 || item.Name == "Sulfuras, Hand of Ragnaros") return;
-
-            if (item.Name == "Aged Brie")
-            {
-                item.Quality = Math.Min(item.Quality + 1, 50);
-            }
-            else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-            {
-                HandleBackstageItemsQuality(item);
-            }
-            else
-            {
-                item.Quality = Math.Max(item.Quality - 1, 0);
-            }
-
-            if (item.SellIn < 0)
-            {
-                if (item.Name == "Aged Brie")
+                if (!uniqueRarities.Contains(item.Name))
                 {
-                    item.Quality = Math.Min(item.Quality + 1, 50);
-                }
-                else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    item.Quality = 0;
-                }
-                else
-                {
-                    item.Quality = Math.Max(item.Quality - 1, 0);
+                    if (item.Quality > 0)
+                    {
+                        item.Quality -= 1;
+                        if (item.Quality < 1)
+                        {
+                            item.Quality = 0;
+                        }
+                    }
                 }
             }
         }
 
-        private void HandleBackstageItemsQuality(Item item)
+        private void UpdateNonUniqueNonRareItemsQuality()
         {
-            item.Quality++;
-
-            if (item.SellIn < 11)
+            foreach (var item in Items)
             {
-                item.Quality++;
-            }
+                if (item.Quality < 50 && !item.IsUniqueRare)
+                {
+                    item.Quality += 1;
 
-            if (item.SellIn < 6)
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (item.SellIn < 11)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality += 1;
+                            }
+                        }
+
+                        if (item.SellIn < 6)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality += 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void UpdateRareItemsQuality()
+        {
+            foreach (var item in Items)
             {
-                item.Quality++;
-            }
+                if (!item.IsUniqueRare)
+                {
+                    if (item.SellIn < 0 && item.Quality > 0)
+                    {
+                        if (item.Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            item.Quality -= item.Quality;
 
-            item.Quality = Math.Min(item.Quality, 50);
+                            if (item.Quality < 1)
+                            {
+                                item.Quality = 0;
+                            }
+                        }
+                    }
+                    if (item.Name == "Aged Brie")
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool IsNonRare(Item item)
+        {
+            return item.Name != "Sulfuras, Hand of Ragnaros" && item.Quality > 0;
         }
     }
 }
