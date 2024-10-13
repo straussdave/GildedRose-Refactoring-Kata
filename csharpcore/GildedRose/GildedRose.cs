83bf6dabf3c48580ddf3db46a63d90859856ad65
straussdave <davidstrauss002@gmai.com>
2024-10-13 15:36:11 +0200

    mistralai/Mistral-Nemo-Instruct-2407​

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -1,83 +1,121 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace GildedRoseKata
 {
     public class GildedRose
     {
-        internal IList<Item> Items;
+        private readonly IList<Item> _items;
 
         public GildedRose(IList<Item> items)
         {
-            this.Items = items;
+            _items = items;
         }
 
         public void UpdateQuality()
         {
-            foreach (var item in Items)
+            foreach (var item in _items)
             {
+                UpdateQualityBasedOnName(item);
                 UpdateSellIn(item);
-                UpdateQuality(item);
+                UpdateQualityAfterSellIn(item);
+            }
+        }
+
+        private void UpdateQualityBasedOnName(Item item)
+        {
+            if (item.IsNormalItem())
+            {
+                DecreaseQuality(item);
+            }
+            else if (item.IsAgedBrieOrBackstagePass())
+            {
+                IncreaseQuality(item, item.IsBackstagePass());
             }
         }
 
         private void UpdateSellIn(Item item)
         {
-            if (item.Name != "Sulfuras, Hand of Ragnaros")
+            if (!item.IsLegendaryItem())
             {
                 item.SellIn--;
             }
         }
 
-        private void UpdateQuality(Item item)
+        private void UpdateQualityAfterSellIn(Item item)
         {
-            if (item.Quality == 0 || item.Name == "Sulfuras, Hand of Ragnaros") return;
-
-            if (item.Name == "Aged Brie")
-            {
-                item.Quality = Math.Min(item.Quality + 1, 50);
-            }
-            else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-            {
-                HandleBackstageItemsQuality(item);
-            }
-            else
-            {
-                item.Quality = Math.Max(item.Quality - 1, 0);
-            }
-
             if (item.SellIn < 0)
             {
-                if (item.Name == "Aged Brie")
+                if (item.IsAgedBrieOrBackstagePass())
                 {
-                    item.Quality = Math.Min(item.Quality + 1, 50);
+                    IncreaseQuality(item, item.IsBackstagePass());
                 }
-                else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                else if (item.IsNormalItem())
                 {
-                    item.Quality = 0;
-                }
-                else
-                {
-                    item.Quality = Math.Max(item.Quality - 1, 0);
+                    DecreaseQuality(item);
                 }
             }
         }
 
-        private void HandleBackstageItemsQuality(Item item)
+        private void IncreaseQuality(Item item, bool isBackstagePass = false)
         {
-            item.Quality++;
+            item.Quality = Clamp(item.Quality + 1, 0, 50);
 
-            if (item.SellIn < 11)
+            if (isBackstagePass && item.SellIn < 11)
             {
-                item.Quality++;
+                item.Quality = Clamp(item.Quality + 1, 0, 50);
             }
 
-            if (item.SellIn < 6)
+            if (isBackstagePass && item.SellIn < 6)
             {
-                item.Quality++;
+                item.Quality = Clamp(item.Quality + 1, 0, 50);
+            }
+        }
+
+        private void DecreaseQuality(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality = Clamp(item.Quality - 1, 0, 50);
+            }
+        }
+
+        private int Clamp(int value, int min, int max)
+        {
+            if (value < min)
+            {
+                return min;
             }
 
-            item.Quality = Math.Min(item.Quality, 50);
+            if (value > max)
+            {
+                return max;
+            }
+
+            return value;
+        }
+    }
+
+    public static class ItemExtensions
+    {
+        public static bool IsNormalItem(this Item item)
+        {
+            return !(item.IsAgedBrieOrBackstagePass() || item.IsLegendaryItem());
+        }
+
+        public static bool IsAgedBrieOrBackstagePass(this Item item)
+        {
+            return item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert";
+        }
+
+        public static bool IsBackstagePass(this Item item)
+        {
+            return item.Name == "Backstage passes to a TAFKAL80ETC concert";
+        }
+
+        public static bool IsLegendaryItem(this Item item)
+        {
+            return item.Name == "Sulfuras, Hand of Ragnaros";
         }
     }
 }
