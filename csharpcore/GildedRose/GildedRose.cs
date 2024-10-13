6ac6fa6eaf93154bce9e5550f83b4b499ab153ef
straussdave <davidstrauss002@gmai.com>
2024-10-13 14:58:17 +0200

    huggingface  starchat

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -1,124 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
-namespace GildedRoseKata;
-
-public class GildedRose
+namespace GildedRoseKata
 {
-    private readonly IList<Item> _items;
-
-    private readonly IList<string> _legendaryItems = new List<string> { "Sulfuras, Hand of Ragnaros" };
-
-    private const string _backStageString = "Backstage passes to a TAFKAL80ETC concert";
-    private const string _brieString = "Aged Brie";
-
-    private readonly int _maxQuality = 50;
-    private readonly int _minQuality = 0;
-
-    public GildedRose(IList<Item> items)
+    public enum ItemType
     {
-        _items = items;
+        Normal,
+        AgedBrie,
+        BackstagePass,
+        Sulfuras
     }
 
-    public void UpdateQuality()
+    public class Item
     {
-        foreach (var item in _items)
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+        public ItemType Type { get; set; }
+    }
+
+    public class GildedRose
+    {
+        private IList<Item> items;
+
+        public GildedRose(IList<Item> items)
         {
-            if (_legendaryItems.Contains(item.Name))
+            this.items = items;
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (var item in items)
             {
-                continue;
+                switch (item.Type)
+                {
+                    case ItemType.Normal:
+                        UpdateNormalItem(item);
+                        break;
+
+                    case ItemType.AgedBrie:
+                        UpdateAgedBrieItem(item);
+                        break;
+
+                    case ItemType.BackstagePass:
+                        UpdateBackstagePassItem(item);
+                        break;
+
+                    case ItemType.Sulfuras:
+                        break; // Sulfuras, Hand of Ragnaros does not change in quality
+
+                }
             }
-            
-            ReduceItemSellIn(item);
+        }
 
-            ChangeQuality(item);
+        private static void UpdateNormalItem(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality = item.SellIn < 0
+                 ? item.Quality - 2
+                  : item.Quality - 1;
+            }
         }
-    }
 
-    private void ChangeQuality(Item item)
-    {
-        var itemName = item.Name;
-        switch (itemName)
+        private static void UpdateAgedBrieItem(Item item)
         {
-            case _brieString:
-                UpdateBrie(item);
-                break;
-            case _backStageString:
-                UpdateBackstagePass(item);
-                break;
-            default:
-                UpdateDefaultItem(item);
-                break;
+            if (item.Quality < 50)
+            {
+                item.Quality++;
+            }
         }
-    }
 
-    private void UpdateDefaultItem(Item item)
-    {
-        var qualityChange = -1;
-        if(item.SellIn < 0)
+        private static void UpdateBackstagePassItem(Item item)
         {
-            qualityChange *= 2;
-        }
-        if (item.Name.Contains("Conjured"))
-        {
-            qualityChange *= 2;
-        }
-        AddQuality(item, qualityChange);
-    }
+            if (item.Quality < 50)
+            {
+                item.Quality++;
 
-    private void UpdateBackstagePass(Item item)
-    {
-        var qualityChange = 0;
-        var sellIn = item.SellIn;
-        if (sellIn < 0)
-        {
-            item.Quality = 0;
-            return;
-        }
-        if (sellIn >= 0)
-        {
-            qualityChange++;
-        }
-        if(sellIn <= 10)
-        {
-            qualityChange++;
-        }
-        if (sellIn <= 5)
-        {
-            qualityChange++;
-        }
-        if(qualityChange != 0)
-        {
-            AddQuality(item, qualityChange);
-        }
-    }
+                if (item.SellIn < 11)
+                {
+                    item.Quality < 50 && item.Quality++;
+                }
 
-    private void UpdateBrie(Item item)
-    {
-        var qualityChange = 1;
-        if(item.SellIn < 0) 
-        { 
-            qualityChange++; 
-        }
-        AddQuality(item, qualityChange);
-    }
+                if (item.SellIn < 6)
+                {
+                    item.Quality < 50 && item.Quality++;
+                }
+            }
 
-    private void ReduceItemSellIn(Item item)
-    {
-        item.SellIn--;
-    }
-
-    private void AddQuality(Item item, int qualityChange)
-    {
-        var newQuality = item.Quality + qualityChange;
-        if(newQuality > _maxQuality)
-        {
-            newQuality = _maxQuality;
+            item.Quality = item.SellIn < 0 ? 0 : item.Quality;
         }
-        if(newQuality < _minQuality)
-        {
-            newQuality = _minQuality;
-        }
-        item.Quality = newQuality;
     }
 }
